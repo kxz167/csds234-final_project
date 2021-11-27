6c8edb6e6080878f8de49fc9a6f676a1bb4c84bc
Escanord <escanord@Duys-MacBook-Pro.local>
2021-11-27 15:29:56 -0500

    add db models
    Signed-off-by: Escanord <escanord@Duys-MacBook-Pro.local>

--- hello/models.py
+++ hello/models.py
@@ -1,5 +1,164 @@
 from django.db import models
 
-# Create your models here.
+
+class AuthGroup(models.Model):
+    name = models.CharField(unique=True, max_length=150)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_group'
+
+
+class AuthGroupPermissions(models.Model):
+    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
+    permission = models.ForeignKey('AuthPermission', models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_group_permissions'
+        unique_together = (('group', 'permission'),)
+
+
+class AuthPermission(models.Model):
+    name = models.CharField(max_length=255)
+    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING)
+    codename = models.CharField(max_length=100)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_permission'
+        unique_together = (('content_type', 'codename'),)
+
+
+class AuthUser(models.Model):
+    password = models.CharField(max_length=128)
+    last_login = models.DateTimeField(blank=True, null=True)
+    is_superuser = models.BooleanField()
+    username = models.CharField(unique=True, max_length=150)
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    email = models.CharField(max_length=254)
+    is_staff = models.BooleanField()
+    is_active = models.BooleanField()
+    date_joined = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user'
+
+
+class AuthUserGroups(models.Model):
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user_groups'
+        unique_together = (('user', 'group'),)
+
+
+class AuthUserUserPermissions(models.Model):
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    permission = models.ForeignKey(AuthPermission, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user_user_permissions'
+        unique_together = (('user', 'permission'),)
+
+
+class Course(models.Model):
+    id = models.IntegerField(primary_key=True)
+    code = models.IntegerField(blank=True, null=True)
+    department = models.ForeignKey('Department', models.DO_NOTHING, blank=True, null=True)
+    name = models.TextField(blank=True, null=True)
+    credits = models.TextField(blank=True, null=True)  # This field type is a guess.
+    description = models.TextField(blank=True, null=True)
+    standing_prequisite = models.TextField(blank=True, null=True)
+    standing_recommendation = models.TextField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'course'
+
+
+class CoursePrerequisite(models.Model):
+    course_id = models.IntegerField(primary_key=True)
+    prerequisite_id = models.IntegerField()
+
+    class Meta:
+        managed = False
+        db_table = 'course_prerequisite'
+        unique_together = (('course_id', 'prerequisite_id'),)
+
+
+class CourseRecommendation(models.Model):
+    course_id = models.IntegerField(primary_key=True)
+    recommendation_id = models.IntegerField()
+
+    class Meta:
+        managed = False
+        db_table = 'course_recommendation'
+        unique_together = (('course_id', 'recommendation_id'),)
+
+
+class Department(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.TextField(blank=True, null=True)
+    abbreviation = models.TextField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'department'
+
+
+class DjangoAdminLog(models.Model):
+    action_time = models.DateTimeField()
+    object_id = models.TextField(blank=True, null=True)
+    object_repr = models.CharField(max_length=200)
+    action_flag = models.SmallIntegerField()
+    change_message = models.TextField()
+    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING, blank=True, null=True)
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'django_admin_log'
+
+
+class DjangoContentType(models.Model):
+    app_label = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+
+    class Meta:
+        managed = False
+        db_table = 'django_content_type'
+        unique_together = (('app_label', 'model'),)
+
+
+class DjangoMigrations(models.Model):
+    app = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    applied = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_migrations'
+
+
+class DjangoSession(models.Model):
+    session_key = models.CharField(primary_key=True, max_length=40)
+    session_data = models.TextField()
+    expire_date = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_session'
+
+
 class Greeting(models.Model):
-    when = models.DateTimeField("date created", auto_now_add=True)
+    when = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'hello_greeting'
